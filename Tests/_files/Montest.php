3c5fa0cfc7b5890381a2b9b1c0d8eb5c8b7b6c44
tuxtux59 <thibaud.renaux@gmail.com>
2013-10-10 11:42:20 +0200

    Fichier test include
    essai

---
+++ Tests/_files/Montest.php
@@ -0,0 +1,13 @@
+<?php
+
+class ExpectedErrorTest extends PHPUnit_Framework_TestCase
+{
+ /**
+ @expectedException PHPUnit_Framework_Error
+ */
+ public function testEchecInclude()
+ {
+ include 'fichier_qui_n_existe_pas.php';
+ }
+}
+?>
